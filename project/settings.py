0f4c32f2af13f05a36739a8b75731cc264a8b34e
Agnes <koinageagness@gmail.com>
2022-06-16 17:05:09 +0300

    configure heroku settings

--- project/settings.py
+++ project/settings.py
@@ -4,6 +4,8 @@ import dj_database_url
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+import django_heroku
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
